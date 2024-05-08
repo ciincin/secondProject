10343db3a645f8ab967abd73c021a386c48f2b88
Cinthya <cinthyaredondo12@gmail.com>
2024-05-08 14:01:25 +0200

    I fix the arrow functionality

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // C A R T
 
 let btnPromo = document.getElementById("cart-promo-button");
-let iconArrow = document.getElementById("cart-icon-down");
+let iconArrow = document.getElementById("cart-icon");
 let promoBox = document.getElementById("cart-promo-open");
 
 let promoBoxIsOpen = false;
 btnPromo.addEventListener("click", () => {
   if (promoBoxIsOpen) {
-    iconArrow.classList.remove("bi-chevron-up");
-    iconArrow.classList.add("bi-chevron-down");
+    iconArrow.classList.remove("rotate-up");
+    iconArrow.classList.add("rotate-down");
     promoBox.classList.add("cart-display-none");
     promoBoxIsOpen = false;
   } else {
-    iconArrow.classList.remove("bi-chevron-down");
-    iconArrow.classList.add("bi-chevron-up");
+    iconArrow.classList.remove("rotate-down");
+    iconArrow.classList.add("rotate-up");
     promoBox.classList.remove("cart-display-none");
     promoBoxIsOpen = true;
   }
