b590f235323af4567d07faa12c4395b2d46a3ece
Cinthya <cinthyaredondo12@gmail.com>
2024-05-08 12:29:11 +0200

    I added the promotion button functionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+// C A R T
+
+//Promotion button
+
+let btnPromo = document.getElementById("cart-promo-button");
+let iconArrow = document.getElementById("cart-icon-down");
+let promoBox = document.getElementById("cart-promo-open");
+
+let promoBoxIsOpen = true;
+btnPromo.addEventListener("click", () => {
+  if (promoBoxIsOpen) {
+    iconArrow.classList.remove("bi-chevron-down");
+    iconArrow.classList.add("bi-chevron-up");
+    promoBox.classList.add("display-none");
+    promoBoxIsOpen=false;
+  } else {
+    iconArrow.classList.remove("bi-chevron-up");
+    iconArrow.classList.add("bi-chevron-down");
+    promoBox.classList.remove("display-none");
+    promoBoxIsOpen=true;
+  }
+});
+
