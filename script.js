f00e53ed1c2db1951089f6f30daba408f9dc9069
Cinthya <cinthyaredondo12@gmail.com>
2024-05-09 16:34:13 +0200

    I added new functionalities

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
 // C A R T
 
-let btnPromo = document.getElementById("cart-promo-button");
-let iconArrow = document.getElementById("cart-icon");
-let promoBox = document.getElementById("cart-promo-open");
 
-let promoBoxIsOpen = false;
-btnPromo.addEventListener("click", () => {
-  if (promoBoxIsOpen) {
-    iconArrow.classList.remove("rotate-up");
-    iconArrow.classList.add("rotate-down");
-    promoBox.classList.add("cart-display-none");
-    promoBoxIsOpen = false;
+// add/remove the same article
+let amountItem = 1;
+
+function decrease(id) {
+  let input = document.getElementById(id);
+
+  if (amountItem > 1) {
+    amountItem--;
+    input.value = amountItem;
+  }
+}
+
+function increase(id) {
+  let input = document.getElementById(id);
+
+  amountItem++;
+
+  input.value = amountItem;
+}
+
+
+//heart button
+
+const btnHeart =document.getElementById("cart-btn-heart")
+const heart = document.getElementById("cart-heart");
+const wishList = document.getElementById("wish-list");
+
+let heartIsNotFilled = true;
+btnHeart.addEventListener("click", () => {
+  if (heartIsNotFilled) {
+    heart.classList.replace("bi-heart", "bi-heart-fill");
+    wishList.textContent = `Quitar de la lista de deseos`;
+    heartIsNotFilled = false;
   } else {
-    iconArrow.classList.remove("rotate-down");
-    iconArrow.classList.add("rotate-up");
-    promoBox.classList.remove("cart-display-none");
-    promoBoxIsOpen = true;
+    heart.classList.replace("bi-heart-fill", "bi-heart");
+    wishList.textContent = `Añadir a la lista de deseos`;
+    heartIsNotFilled = true;
+  }
+});
+
+//promo code button
+
+const btnPromo = document.getElementById("cart-promo-button");
+const iconArrow = document.getElementById("cart-icon");
+const promoBox = document.getElementById("cart-promo-open");
+
+let promoBoxIsClosed = true;
+btnPromo.addEventListener("click", () => {
+  promoBox.classList.toggle("cart-display-none");
+
+  if (promoBoxIsClosed) {
+    iconArrow.classList.replace("rotate-down", "rotate-up");
+    promoBoxIsClosed = false;
+  } else {
+    iconArrow.classList.replace("rotate-up", "rotate-down");
+    promoBoxIsClosed = true;
   }
 });
