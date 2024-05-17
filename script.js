10ba94a2ba0cff076837095ba51f28101048d172
Victor M <victormilite96@gmail.com>
2024-05-17 13:02:55 +0200

    ya se agregan las cards dinamicamente

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let cartList = [
   {
     id: 0,
-    image: "assets/disney-sets/cabañaBlancanieves/cabaña600x450.webp",
+    image: "assets/disney-sets/cabañaBlancanieves/cabaña-600x450.webp",
     title: "Cabaña de Blancanieves y los Siete Enanitos",
     price: 219.99,
     amount: 1,
@@ -11,7 +11,7 @@ let cartList = [
   },
   {
     id: 1,
-    image: "assets/disney-sets/castilloDisney/disney417x600.webp",
+    image: "assets/disney-sets/castilloDisney/disney-417x600.webp",
     title: "Castillo Disney",
     price: 399.99,
     amount: 1,
@@ -21,7 +21,7 @@ let cartList = [
   },
   {
     id: 2,
-    image: "assets/disney-sets/frozen/frozen600x450.webp",
+    image: "assets/disney-sets/frozen/frozen-600x450.webp",
     title: "Palacio de Hielo de Elsa",
     price: 99.99,
     amount: 1,
@@ -31,7 +31,7 @@ let cartList = [
   },
   {
     id: 3,
-    image: "assets/disney-sets/insideOut/insideOut600x400.webp",
+    image: "assets/disney-sets/insideOut/insideOut-600x400.webp",
     title: "Inside Out 2 Mood Cubes",
     price: 34.99,
     amount: 1,
@@ -41,7 +41,7 @@ let cartList = [
   },
   {
     id: 4,
-    image: "assets/disney-sets/reyLeon/reyLeon600x450.webp",
+    image: "assets/disney-sets/reyLeon/reyLeon-600x450.webp",
     title: "El Rey León: Simba Cachorro",
     price: 19.99,
     amount: 1,
@@ -51,7 +51,7 @@ let cartList = [
   },
   {
     id: 5,
-    image: "assets/disney-sets/sirenita/sirenita600x450.webp",
+    image: "assets/disney-sets/sirenita/sirenita-600x450.webp",
     title: "Mini Castillo de Disney Ariel",
     price: 39.99,
     amount: 1,
@@ -61,7 +61,7 @@ let cartList = [
   },
   {
     id: 6,
-    image: "assets/disney-sets/stitch/stitch600x450.webp",
+    image: "assets/disney-sets/stitch/stitch-600x450.webp",
     title: "Stitch",
     price: 64.99,
     amount: 1,
@@ -71,7 +71,7 @@ let cartList = [
   },
   {
     id: 7,
-    image: "assets/disney-sets/up/up600x450.webp",
+    image: "assets/disney-sets/up/up-600x450.webp",
     title: "Casa de Up",
     price: 54.99,
     amount: 1,
@@ -81,7 +81,7 @@ let cartList = [
   },
   {
     id: 8,
-    image: "assets/disney-sets/wall-e/wall-e600x450.webp",
+    image: "assets/disney-sets/wall-e/walle-600x450.webp",
     title: "EVA y WALL•E",
     price: 14.99,
     amount: 1,
@@ -92,8 +92,60 @@ let cartList = [
 ];
 
 // D I S N E Y  S E T S //
+const disneyLayout = document.getElementById("disney-set-layout")
 
+function disneySetsTemplate(title, image, price, age, pieces){
+  let disneyTemplateItem = `
+  <div class="disney-card1">
+                <div class="disney-card1-likeButton-banner"></div>
+                <div class="disney-card1-imageSet-container">
+                    <!-- aqui es donde va el carrousel -->
+                    <img class="disney-card1-imageSet" src=${image}
+                        alt=${title} />
+                </div>
+                <div class="disney-card1-infoSet-container">
+                    <div class="disney-icon-container">
+                        <img  class="disney-age-icon" src="assets/disney-sets/age-o.svg" alt="age-icon">
+                    </div>
+                    <div class="disney-textIcon-container">
+                        <span class="disney-age-text">${age}+</span>
+                    </div>
+                    <div class="disney-icon-container">
+                        <img  class="disney-age-icon" src="assets/disney-sets/brick-o.svg" alt="age-icon">
+                    </div>
+                    <div class="disney-textIcon-container">
+                        <span class="disney-age-text">${pieces}</span>
+                    </div>
+                </div>
+                <div class="disney-card1-titleSet-container">
+                    <span>${title}</span>
+                </div>
+                <div class="disney-card1-priceSet-container">
+                    <span><b>${price}</b><i class="bi bi-currency-euro"></i></span>
+                </div>
+                <div class="disney-card1-addToCart-container">
+                    <button type="button" class="btn btn-primary disney-btn-addToCart">
+                        <div class="disney-card1-addToCart-innerContainer">
+                            <div class="disney-bagIcon-container">
+                                <img  class="disney-bag-icon" src="assets/disney-sets/shopping-bag-o.svg" alt="age-icon">
+                            </div>
+                            <span class="disney-btn-addToCart-text">Añadir a la Bolsa</span>
+                        </div>
+                    </button>
+                </div>
+            </div>`;
+            return disneyTemplateItem;
+}
 
+cartList.forEach(item => {
+  disneyLayout.innerHTML += disneySetsTemplate(
+    item.title,
+    item.image,
+    item.price,
+    item.minAge,
+    item.pieces
+  )
+})
 
 // C A R T
 
